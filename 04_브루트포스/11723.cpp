7484a511eb029570ef2a180671854cbf18f95e4a
Songein <songein@ewhain.net>
2023-09-13 03:31:41 +0900

    [브루트포스] 9월 13일

---
+++ 04_브루트포스/11723.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <unordered_map>
+int const bi_all = 2097150; // 1111 1111 1111 1111 1111
+int const bi_empty = 0;
+
+using namespace std;
+
+void cal(string s, int& ans) {
+	int n;
+
+	if (s == "add") {
+		cin >> n;
+		ans |= (1 << n);
+	}
+	else if (s == "remove") {
+		cin >> n;
+		ans &= ~(1 << n);
+	}
+	else if (s == "check") {
+		cin >> n;
+		if ((ans & (1 << n)) != 0) {
+			cout << 1 << "\n";
+		}
+		else {
+			cout << 0 << "\n";
+		}
+	}
+	else if (s == "toggle") {
+		cin >> n;
+		ans ^= (1 << n);
+	}
+	else if (s == "all") {
+		ans |= bi_all;
+	}
+	else if (s == "empty") {
+		ans &= bi_empty;
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	//선언
+	int m, c, ans;
+	string s;
+	ans = 0;
+
+
+	//입력
+	cin >> m;
+	
+	//연산
+	for (int i = 0; i < m; i++) {
+		cin >> s;
+		cal(s, ans);
+	}
+}
