61fa8d508604057587ca41738634f12f08e36b19
Songein <songein@ewhain.net>
2023-10-11 05:07:35 +0900

    [DFS, BFS] 10월 11일

---
+++ 08_DFS_BFS/1325.cpp
@@ -0,0 +1,69 @@
+﻿#include <iostream>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+int dfs(vector<vector<int>> &computer, int &n, int &c) {
+	stack<int> s;
+	vector<bool> visit(n+1, false); //방문한 컴퓨터 체크
+	int count, index, temp;
+	bool child = false;
+
+	s.push(c); //c번째 컴퓨터부터 시작
+	visit[c] = true;
+	count = 0;
+
+	while (!s.empty()) {
+		index = s.top(); //stack 제일 위쪽의 index를 저장
+		//child = false;
+		s.pop(); // 꺼냄
+		count++; // 카운트를 올림
+
+		for (int i = 0; i < computer[index].size(); i++) { //index와 연결된 노드들 확인
+			temp = computer[index][i];
+			if (!visit[temp]) { // 방문한 적이 없을 경우
+				s.push(temp); // 스택에 추가
+				visit[temp] = true; // 이후 방문을 true로
+			}
+		}
+	}
+	return count;
+}
+
+int main() {
+	//입출력 속도 향상
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	//선언
+	int n, m, a, b, max, temp;
+	max = 1;
+	vector<int> ans;
+
+	//입력
+	cin >> n >> m;
+	vector<vector<int>> computer(n+1);
+	for (int i = 0; i < m; i++) {
+		cin >> a >> b;
+		computer[b].push_back(a); //b를 기준으로 b를 신뢰하는 a를 벡터에 나열
+	}
+
+	//연산
+	for (int i = 1; i <= n; i++) {
+		temp = dfs(computer, n, i);
+		if (max < temp) {
+			ans.clear();
+			ans.push_back(i);
+			max = temp;
+		}
+		else if (max == temp) {
+			ans.push_back(i); //i가 작은 것부터 순서대로 확인하므로 오름차순으로 정렬됨.
+		}
+	}
+
+	//출력
+	for (int i = 0; i < ans.size(); i++) {
+		cout << ans[i] << " ";
+	}
+}
---
+++ 08_DFS_BFS/4963.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <vector>
+const int DXY[8][8] = { {-1,0},{-1,1},{-1,-1},{0,1},{0,-1},{1,0 }, {1,1}, {1,-1} }; //8방향
+
+using namespace std;
+
+void dfs(vector<vector<int>>& v, vector<vector<bool>>& visit, int h, int w, int i, int j) {
+	visit[i][j] = true;
+	int x, y;
+
+	for (int k = 0; k < 8; k++) {
+		x = i + DXY[k][0];
+		y = j + DXY[k][1];
+
+		if ((x >= 0 && x < h) && (y >= 0 && y < w)) {
+			if (v[x][y] == 1 &&  !visit[x][y]) { //섬이면서 방문하지 않았을 때
+				dfs(v, visit, h, w, x, y);
+			}
+		}
+	}
+
+}
+
+int main() {
+	//선언
+	int w, h, ans = 0;
+
+	//입력, 연산 및 출력
+	while (true) {
+		cin >> w >> h;
+		if (w == 0 && h == 0) {
+			break;
+		}
+		vector<vector<int>> v(h, vector<int>(w));
+		vector<vector<bool>> visit(h, vector<bool>(w, false));
+		for (int i = 0; i < h; i++) {
+			for (int j = 0; j < w; j++) {
+				cin >> v[i][j];
+			}
+		}
+		for (int i = 0; i < h; i++) {
+			for (int j = 0; j < w; j++) {
+				// 땅이고(1) 방문하지 않았을 때
+				if (v[i][j] == 1 && !visit[i][j]) {
+					ans++;
+					dfs(v, visit, h, w, i, j);
+				}
+			}
+		}
+		cout << ans << "\n";
+		ans = 0;
+	}
+}
