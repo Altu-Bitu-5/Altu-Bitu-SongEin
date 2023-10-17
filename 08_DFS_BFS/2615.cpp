bc7e11d0c61eefbcb498beb3bfa2a7ce0438b10a
Songein <songein@ewhain.net>
2023-10-17 17:24:28 +0900

    Update 2615.cpp

--- 08_DFS_BFS/2615.cpp
+++ 08_DFS_BFS/2615.cpp
@@ -14,40 +14,34 @@ void omock(int board[SIZE][SIZE], int ans[]) {
 			if (board[i][j] == 0) { // 돌이 없으면 continue
 				continue;
 			}
-			else { // 돌이 있을 경우
-				pl = board[i][j];
-				for (int k = 0; k < 4; k++) {
-					// 역방향에 돌이 놓여있었을 경우 (이미 체크한 경우) 확인
-					x = i - dir[k][0];
-					y = j - dir[k][1];
-					if (((x >= 0 && x < 19) && (y >= 0 && y < 19)) && board[x][y] == pl) {
-						continue; //건너뜀
+			// 돌이 있을 경우
+			pl = board[i][j];
+			for (int k = 0; k < 4; k++) {
+				// 역방향에 돌이 놓여있었을 경우 (이미 체크한 경우) 확인
+				x = i - dir[k][0];
+				y = j - dir[k][1];
+				if (((x >= 0 && x < 19) && (y >= 0 && y < 19)) && board[x][y] == pl) {
+					continue; //건너뜀
+				}
+				//역방향에 돌이 없을 경우
+				x = i;
+				y = j;
+				count = 1;
+				while (true) {
+					x += dir[k][0];
+					y += dir[k][1];
+					if (((x >= 0 && x < 19) && (y >= 0 && y < 19))&& board[x][y] == pl) { // 방향으로 전진하는 동안 같은 말이 반복될 경우
+						count++;
 					}
-					//역방향에 돌이 없을 경우
-					x = i;
-					y = j;
-					count = 1;
-					while (true) {
-						x += dir[k][0];
-						y += dir[k][1];
-						if ((x >= 0 && x < 19) && (y >= 0 && y < 19)) {
-							if (board[x][y] == pl) { // 방향으로 전진하는 동안 같은 말이 반복될 경우
-								count++;
-							}
-							else {
-								break;
-							}
-						}
-						else {
-							break;
-						}
+					else {
+						break;
 					}
-					if (count == 5) { // count가 5일 경우
-						ans[0] = pl;
-						ans[1] = i+1;
-						ans[2] = j+1;
-						return;
-					}					
+				}
+				if (count == 5) { // count가 5일 경우
+					ans[0] = pl;
+					ans[1] = i + 1;
+					ans[2] = j + 1;
+					return;
 				}
 			}
 		}
