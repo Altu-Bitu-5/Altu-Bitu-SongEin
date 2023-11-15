553c52ddd6812bcca76f138d6b6c609cf2e69dab
Songein <songein@ewhain.net>
2023-11-15 16:17:25 +0900

    [투포인터] 11월 15일

---
+++ 11_투포인터/20922.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int const  N_max = 200000;
+int a_visit[N_max];
+
+int slide(vector<int> &a, int *a_visit, int k, int n) {
+	int count = 0;
+	int index = 0;
+	int ans = 0;
+
+	for (int i = 0; i < n; i++) {
+		a_visit[a[i]]++;
+		count++;
+		while (a_visit[a[i]]> k) { //추가해야하는 슬라이더 번호의 중복 개수가 k개보다 많을 경우
+			a_visit[a[index]]--;
+			index++; // 슬라이더 앞 인덱스 증가
+			count--; // 개수 감소
+		}
+		ans = max(ans, count);
+	}
+
+	return ans;
+}
+
+int main() {
+	//입출력 속도 향상
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	//선언
+	int n, k;
+
+	//입력
+	cin >> n >> k;
+	vector<int> a(n);
+
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+
+	//연산 및 출력
+	cout << slide(a, a_visit, k, n);
+}
---
+++ 11_투포인터/2531.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int slide(vector<int> &sushi, vector<int> &sushi_visit, int k, int c, int n) {
+	int count = 0;
+	int ans = 0;
+
+	for (int i = 0; i < k; i++) {
+		if (sushi_visit[sushi[i]] == 0) {
+			count++;
+		}
+		sushi_visit[sushi[i]]++;
+	}
+	if (sushi_visit[c] == 0) {
+		ans = count + 1;
+	}
+	else {
+		ans = count;
+	}
+	
+
+	for (int i = 0; i < n; i++) {
+		//슬라이더 앞쪽
+		sushi_visit[sushi[i]]--;
+		if (sushi_visit[sushi[i]] == 0) {
+			count--;
+		}
+		//슬라이더 뒤쪽
+		if (sushi_visit[sushi[(i + k) % n]] == 0) {
+			count++;
+		}
+		sushi_visit[sushi[(i + k) % n]]++;
+
+		if (sushi_visit[c] == 0) { //쿠폰 확인
+			ans = max(count + 1, ans);
+		}
+		else {
+			ans = max(count, ans);
+		}
+	}
+
+	return ans;
+}
+
+int main() {
+	//선언
+	int n, d, k, c, ans;
+
+	//입력
+	cin >> n >> d >> k >> c;
+	vector<int> sushi(n); //초밥 접시
+	vector<int> sushi_visit(d+1); //초밥 종류
+
+	for (int i = 0; i < n; i++) {
+		cin >> sushi[i];
+	}
+
+	//연산 및 출력
+	cout << slide(sushi, sushi_visit, k, c, n);
+}
